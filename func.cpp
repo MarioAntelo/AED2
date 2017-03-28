a6b0dac9973193c9f93fd4d089d6e63a56ee9b55
Mario Antelo Ribera <mario.antribera@gmail.com>
2017-03-28 20:01:47 +0200

    jajajajaja

--- func.cpp
+++ func.cpp
@@ -1,3 +1,18 @@
 #include "func.h"
 
 using namespace std;
+
+RES dvd_subcad(CADENA cad, int inicio, int final){
+	int caso_base, mitad;
+
+	//caso base
+	caso_base = cad.getSize_Subcad();
+	if( .... <= cad.get)
+		cad.comprobar_Subcad(inicio, final);
+	else{
+		int mitad = 
+		dvd_subcad(...);
+		dvd_subcad(....);
+		dvd_combinar(.....);
+	}
+}
