3db3da296c838b6def188a56cf83cb756c04207d
Unknown <deline2735@gmail.com>
2017-04-07 11:42:32 +0200

    Iter

--- iterativo.cpp
+++ iterativo.cpp
@@ -1,41 +1,39 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include "cadena.h"
+#include "func.h"
 
 using namespace std;
 
-string a, b; 
-int m;
-int main(int argc, char*argv[]){
 
+int main(int argc, const char* argv[]){
+	
+	if (argc != 4) {
+		cout << "El numero de argumentos introducidos no es el correcto" << endl;
+		cout << "Modo de funcionamiento: main cadena tamaño_subcadena caracter" << endl;
+		return -1;
+	}
+	
+	string str_cad = argv[1];
+	int tam_subcad = atoi(argv[2]);
+	char caracter = *argv[3];
+	int apariciones = 0;
+	int max = 0;
+	int indice = 0;
+	int i = 0;
+	
+	CADENA cadena(str_cad, tam_subcad, caracter);
 
-	string txt;
-	txt = argv[1];
-	ifstream fichero;
-	fichero.open (txt.c_str());
-	int num_casos;
-	fichero >> num_casos;
-	for (int i = 0; i < num_casos; i++){
-		fichero >> m; fichero >> a; fichero >> b;
-		int posMax=-1;
-		int valorMax=-1;
-		int origen=0;
-		int valorActual;
-		while(origen+m <= a.length()){
-			for (int j = 0; j < m; ++j ){
-				valorActual+=abs(int(a[origen+j]) -int(b[origen+j]));
-			}
+	cadena.print();
 
-			if (valorActual > valorMax){
-				posMax=origen;
-				valorMax=valorActual;
-			}
-
-			origen++;
-
+	int n = cadena.getSize()-tam_subcad;
+	for (i=0; i< n; i++){
+		apariciones= cadena.comprobar_Subcad(i, i + tam_subcad);
+		if(apariciones>max){
+			max = apariciones;
+			indice = i;
 		}
-		cout << posMax << " " << valorMax << endl;
+
 	}
 
+	cout << "solucion==> apariciones: "<< max << " empieza:" << indice << endl;
+	return 0;
 }
