962bf5af9523c67164072676da2e7e0439fc1185
Mario Antelo Ribera <mario.antribera@gmail.com>
2017-04-07 18:33:39 +0200

    pruebas experimento

--- experimento.cpp
+++ experimento.cpp
@@ -2,16 +2,17 @@
 #include <iomanip> 
 #include "generador.h"
 #include "cadena.h"
+#include <sys/time.h>
 
 using namespace std;
 void experimento(CADENA cad);
-
+double timeval_diff(struct timeval *a, struct timeval *b);
 
 int main(int argc, const char* argv[]){
 	
-	if (argc != 3) {
+	if (argc != 4) {
 		cout << "El numero de argumentos introducidos no es el correcto" << endl;
-		cout << "Modo de funcionamiento: main cantidad_cadenas_generar caracter" << endl;
+		cout << "Modo de funcionamiento: main cantidad_cadenas_generar caracter inicio_tam_cadena" << endl;
 		cout << "la generacion de cadenas se iniciara en 10 y su incremento sera el doble del tamaño anterior" << endl;
 		return -1;
 	}
@@ -19,22 +20,24 @@ int main(int argc, const char* argv[]){
 	int cant_cad = atoi(argv[1]);
 	int tam_subcad = 0;
 	char caracter = *argv[2];
+
+	//tamaño de m 
+	int a[] = {5, 10, 50, 100, 250, 500,1000};
 	
-	int ini = 1000;
-	for (int i = 0; i <= cant_cad; i++) {
+	int ini = atoi(argv[3]);
+	for (int i = 0; i < cant_cad; i++) {
 		string str = generador(ini);
 		CADENA cadena(str, tam_subcad, caracter);
-		cadena.print();
-		cout << "tam: "<< cadena.getSize() << endl;
+		//cadena.print();
+		cout << endl << "n: "<< cadena.getSize() << endl;
 
-		//se hace el experimento con  4 tamaño de m
-		double porcentaje = 1;
-		for (int j = 0; j < 4 ; j++){
-			tam_subcad = cadena.getSize() * porcentaje;
-			cout << "tamano m: " << tam_subcad<< endl;
-			cadena.set_subcad(tam_subcad);
-			experimento(cadena);
-			porcentaje /= 2; 
+		for (int j = 0; j < 7 ; j++){
+			if(a[j] <= cadena.getSize()){
+				tam_subcad = a[j];
+				cout << "m: " << tam_subcad<< endl;
+				cadena.set_subcad(tam_subcad);
+				experimento(cadena);
+			}
 				
 		}
 		
@@ -46,20 +49,25 @@ int main(int argc, const char* argv[]){
 	
 void experimento(CADENA cad) {
 
-	clock_t start;
-	time_t now;
-	
+	struct timeval t_ini, t_fin;
+  	double secs;
+  	double resultado = 0.0;
 	
 	solucion res;
 	for (int i = 0; i < 10; ++i){
-		start = clock();
-		now = time(NULL);
-		
+		gettimeofday(&t_ini, NULL);   // Instante inicial
 		res = dvd_subcad(cad, 0, cad.getSize()-1);
-		cout << "DyV Time: " <<(clock() - start) / (double)(CLOCKS_PER_SEC) << " ms" << endl;
-	}
-	
+	    gettimeofday(&t_fin, NULL);  // Instante final
 
-	cout << "solucion==> apariciones: "<< res.max;
-	cout<< " empieza:" << res.ini_subcad << endl;
+	    secs = timeval_diff( &t_ini, &t_fin);
+	    resultado += secs;
+	}
+	resultado /=10;
+	cout << resultado << endl;
+}
+
+/* retorna "a - b" en segundos */
+double timeval_diff(struct timeval *a, struct timeval *b)
+{
+  return  (b->tv_sec - a->tv_sec)*1000 + (b->tv_usec - a->tv_usec)/1000.0; 
 }
